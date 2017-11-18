b904a8922527ea856af5c430762a693625bcb349
Lucas Leandro <lucasleandro1204@gmail.com>
2017-11-18 02:25:43 -0200

    working?

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './build'),
     publicPath: '/build/',
-    filename: '[name].js'
+    filename: 'js/app.js'
   },
   module: {
     rules: [
