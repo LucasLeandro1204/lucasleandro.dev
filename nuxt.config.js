48e0cf7d4d1cf4c7fc3cb258acc112a2c58552cb
LucasLeandro1204 <lucasleandro1204@gmail.com>
2019-01-07 20:56:35 -0200

    add custom icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,11 +31,27 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Lucas Leandro - Developer Freelance' }
+      { hid: 'description', name: 'description', content: 'Lucas Leandro - Developer Freelance' },
+      { name: 'msapplication-TileColor', content: '#ffffff' },
+      { name: 'msapplication-TileImage', content: '/icon/ms-icon-144x144.png' },
+      { name: 'theme-color', content: '#ffffff' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: 'apple-touch-icon', sizes: '57x57', href: '/icon/apple-icon-57x57.png', },
+      { rel: 'apple-touch-icon', sizes: '60x60', href: '/icon/apple-icon-60x60.png', },
+      { rel: 'apple-touch-icon', sizes: '72x72', href: '/icon/apple-icon-72x72.png', },
+      { rel: 'apple-touch-icon', sizes: '76x76', href: '/icon/apple-icon-76x76.png', },
+      { rel: 'apple-touch-icon', sizes: '114x114', href: '/icon/apple-icon-114x114.png', },
+      { rel: 'apple-touch-icon', sizes: '120x120', href: '/icon/apple-icon-120x120.png', },
+      { rel: 'apple-touch-icon', sizes: '144x144', href: '/icon/apple-icon-144x144.png', },
+      { rel: 'apple-touch-icon', sizes: '152x152', href: '/icon/apple-icon-152x152.png', },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/icon/apple-icon-180x180.png', },
+      { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/icon/android-icon-192x192.png', },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/icon/favicon-32x32.png', },
+      { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/icon/favicon-96x96.png', },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/icon/favicon-16x16.png', },
+      { rel: 'manifest', href: '/manifest.json', }
+    ],
   },
 
   /*
@@ -61,7 +77,6 @@ module.exports = {
   */
   modules: [
     '@nuxtjs/sitemap',
-
 
     ['@nuxtjs/google-analytics', {
       id: 'UA-109495192-4',
