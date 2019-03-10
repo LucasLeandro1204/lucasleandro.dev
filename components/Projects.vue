4357f6c6d6b21fbd5a620dfe6552bf7b00e4ff32
LucasLeandro1204 <lucasleandro1204@gmail.com>
2019-03-10 19:27:56 -0300

    projects array

--- components/Projects.vue
+++ components/Projects.vue
@@ -10,10 +10,35 @@
 
 <script>
   import Title from './Title';
+  import Innbox from '~/assets/projects/innbox.svg';
+  import Convertpack from '~/assets/projects/convertpack.svg';
 
   export default {
     components: {
       Title,
+    },
+
+    computed: {
+      projects: () => ([
+        {
+          logo: Convertpack,
+          title: 'Convertpack',
+          background: 'bg-gradient-blue',
+          description: 'A package of tools to boost your sales',
+          stack: [
+            'Laravel', 'Vue',
+          ],
+        },
+        {
+          logo: Innbox,
+          title: 'Innbox',
+          background: 'bg-gradient-green',
+          description: 'Hotel & Hostel website',
+          stack: [
+            'Nuxt',
+          ],
+        },
+      ]),
     },
   };
 </script>
