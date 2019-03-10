9b4504353183eb246f3a995f59be714fdb6f26da
LucasLeandro1204 <lucasleandro1204@gmail.com>
2019-03-10 02:26:41 -0300

    title component

---
+++ components/Title.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="relative text-center">
+    <span
+      v-text="background"
+      :class="light ? 'text-white' : 'text-grey-lighter'"
+      class="text-12xl select-none font-extrabold"
+      >
+    </span>
+
+    <div class="absolute pin-t pin-l items-center justify-center flex w-full h-full">
+      <span class="border-t border-pink w-20 mr-4"></span>
+      <h2 v-text="title"></h2>
+      <span class="border-t border-pink w-20 ml-4"></span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      title: {
+        type: String,
+        required: true,
+      },
+
+      background: {
+        type: String,
+        required: true,
+      },
+
+      light: {
+        type: Boolean,
+        required: false,
+        default: false,
+      },
+    },
+  };
+</script>
